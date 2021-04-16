c14832ec83d279a242b9ad47804017f2f261d848
Parker Morris <pmorris@ceiamerica.com>
2021-04-16 15:25:48 -0400

    Finished Part 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,5 +10,44 @@ namespace GradeBook.GradeBooks
 		{
 			Type = GradeBookType.Ranked;
 		}
-	}
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+			if(Students.Count < 5)
+            {
+				throw new InvalidOperationException("Must have more than 5 students to grade.");
+            }
+
+            //fix later
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade >= 80)
+                return 'B';
+            else if (averageGrade >= 70)
+                return 'C';
+            else if (averageGrade >= 60)
+                return 'D';
+            else
+                return 'F';          
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Must have more than 5 students to grade.");
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Must have more than 5 students to grade.");
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+    }
 }
